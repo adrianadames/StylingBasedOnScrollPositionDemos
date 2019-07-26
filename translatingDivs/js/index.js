971f7dbd33e8f93c712a0a150517cb73ad81179b
adrian <adrianadames@gmail.com>
2019-07-26 00:51:43 -0400

    Got a div moving horizontally while simulatenously moving another div vertically.

--- translatingDivs/js/index.js
+++ translatingDivs/js/index.js
@@ -4,11 +4,17 @@ console.log('outerContainer: ', outerContainer);
 let view1 = document.querySelector('.view1');
 console.log('view1: ', view1);
 
-let view1Header = document.querySelector('.view1-header');
-console.log('view1Header: ', view1Header);
+let view1Header1 = document.querySelector('.view1-header1');
+console.log('view1Header1: ', view1Header1);
 
-let view1HeaderPosition = document.querySelector('.view1-header-position');
-console.log('view1HeaderPosition: ', view1HeaderPosition);
+let view1Header2 = document.querySelector('.view1-header2');
+console.log('view1Header2: ', view1Header2);
+
+let view1Header3 = document.querySelector('.view1-header3');
+console.log('view1Header3: ', view1Header3);
+
+// let view1HeaderPosition = document.querySelector('.view1-header-position');
+// console.log('view1HeaderPosition: ', view1HeaderPosition);
 
 // view1Header.addEventListener('click', () => {
 //     console.log('beep')
@@ -33,10 +39,20 @@ console.log('body: ', body);
 window.addEventListener('scroll', () => {
     setTimeout(500)
     console.log('beep')
-    view1HeaderPosition.textContent = 'window.pageYOffset: ' + window.pageYOffset;
-    view1Header.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,${window.pageYOffset},0,0,1)`;
-    view2.style.transform = `translateY(${2360-window.pageYOffset}) translateZ(0px)`;
+    view1Header1.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,-${window.pageYOffset},0,1)`;
+    view1Header2.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,${window.pageYOffset},0,0,1)`;
+
+    // view2.style.transform = `translateY(${2360-window.pageYOffset}) translateZ(0px)`;
+    view2.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,${window.pageYOffset},0,1)`;
 })
+
+// window.addEventListener('wheel', () => {
+//     console.log('wheel')
+//     // view1HeaderPosition.textContent = 'window.pageYOffset: ' + window.pageYOffset;
+//     // view1Header.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,${window.pageYOffset},0,0,1)`;
+//     view2.style.transform = `translateY(${2360-window.pageYOffset})`;
+//     console.log('view2.style.transform: ', `translateY(${2360-window.pageYOffset})`)
+// })
 
 
 
