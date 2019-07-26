ebf48bacc2072ad7b9251cc3ea16e01daa53831e
adrian <adrianadames@gmail.com>
2019-07-26 04:27:28 -0400

    Got view2 to translate up into viewport and view1 to translate up out of viewport starting when view2 just appears in the viewport. Next task that I just started to work in is translating the divs in view2 right when view1 is no longer visible in the viewport.

--- translatingDivs/js/index.js
+++ translatingDivs/js/index.js
@@ -4,6 +4,9 @@ console.log('outerContainer: ', outerContainer);
 let view1 = document.querySelector('.view1');
 console.log('view1: ', view1);
 
+let view1Measure = document.querySelector('.view1-measure');
+console.log('view1Measure: ', view1Measure);
+
 let view1Header1 = document.querySelector('.view1-header1');
 console.log('view1Header1: ', view1Header1);
 
@@ -36,14 +39,33 @@ console.log('scrollTop2: ', scrollTop2);
 let body = document.querySelector('body');
 console.log('body: ', body);
 
+let vpHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+console.log('vpHeight: ', vpHeight);
+let v1Height = 2360;
+
+
+// let view1ContentBounds = document.querySelector('.view1-content-bounds');
+// console.log('view1ContentBounds: ', view1ContentBounds);
+
 window.addEventListener('scroll', () => {
     setTimeout(500)
     console.log('beep')
-    view1Header1.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,-${window.pageYOffset},0,1)`;
-    view1Header2.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,${window.pageYOffset},0,0,1)`;
-
+    
+    // view1ContentBounds.style.top = `${-window.pageYOffset}px`;
+    // view1Header1.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,-${window.pageYOffset},0,1)`;
+    view1Header1.style.transform =`translateY(${-window.pageYOffset}px)`;
+    // view1Header2.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,${window.pageYOffset},0,0,1)`;
+    view1Header2.style.transform =`translateX(${window.pageYOffset}px)`;
     // view2.style.transform = `translateY(${2360-window.pageYOffset}) translateZ(0px)`;
-    view2.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,${window.pageYOffset},0,1)`;
+    // view2.style.transform = `matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,${2360-window.pageYOffset},0,1)`;
+    view2.style.transform = `translateY(${2360-window.pageYOffset}px)`;
+    view1Measure.textContent = 'View 1 Measure (window.pageYOffset: ' + window.pageYOffset +'px)';
+
+
+    if (amountBeforeNextViewAppears(vpHeight,v1Height) <= 0) {
+        console.log('start scrolling current view up');
+        view1.style.transform = `translateY(${amountBeforeNextViewAppears(vpHeight,v1Height)}px)`;
+    }
 })
 
 // window.addEventListener('wheel', () => {
@@ -55,5 +77,11 @@ window.addEventListener('scroll', () => {
 // })
 
 
+
+function amountBeforeNextViewAppears(viewportHeight,currentViewHeight) {
+    let totalAmount = currentViewHeight - viewportHeight;
+    let remainingAmount = totalAmount - window.pageYOffset;
+    return remainingAmount;
+}
 
 
